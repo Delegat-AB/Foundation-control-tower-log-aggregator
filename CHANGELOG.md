76a7586718cc371017bd351eb323b5a42da5a442
Peter Bengtson <peter@peterbengtson.com>
2023-12-29 10:16:48 +0100

    Release v1.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,30 @@
 # Change Log
 
+## v1.2.1
+    * Documented the new features in the README.
+
+## v1.2.0
+    * Auxiliary log file copy strategy used when all log files are > 200K.
+    * Bulk deletes now used to speed up deletion of log file originals.
+    * State machine to process historical logs. See template.yaml for details.
+
+## v1.1.1
+    * Fixed continuation bug: now the aggregation can handle any number and any size of log files.
+
+## v1.1.0
+    * Updated the state machine graphic for the new flow.
+
+## v1.0.9
+    * Continuation mechanism in place to avoid lambda timeouts for large log files
+      or large amounts of log files.
+
+## v1.0.8
+    * Flow change: first the main logs, then the additional logs. Parallelism in the
+      latter temporarily set to 1 and debug printouts added.
+
+## v1.0.7
+    * The filler file is now exactly 5MB in size and sparse.
+
 ## v1.0.6
     * Fixed the 1B bug when aggregation regions are specified.
 
--- functions/combine_log_files/app.py
+++ functions/combine_log_files/app.py
@@ -1,6 +1,13 @@
 import boto3
 import os
 import json
+import time
+import logging
+
+# Configure the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 FIVE_MB = 5 * 1024 * 1024
 
@@ -10,52 +17,70 @@ DEST_LOGS_BUCKET_NAME = os.environ['DEST_LOGS_BUCKET_NAME']
 AGGREGATION_REGIONS = os.environ.get('AGGREGATION_REGIONS', "[]")
 AGGREGATION_REGIONS = json.loads(AGGREGATION_REGIONS.replace("'", '"'))
 
+
 # Create the 5MB file at lambda startup time
 filler_file_path = f'/tmp/five_mb_file'
 with open(filler_file_path, 'wb') as f:
-    f.seek(FIVE_MB)
-    f.write(b'0')
+    f.seek(FIVE_MB - 1)
+    f.write(b'\x00')
 
 s3_client = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 
 
-def lambda_handler(data, _context):
+def lambda_handler(data, context):
     source_bucket_name = data['bucket_name']
     dest_bucket_name = DEST_LOGS_BUCKET_NAME or source_bucket_name
     final_key = data['key']
     log_files = get_files(data['files'])
     main_log_type = data.get('log_type')   # This is only true for a main log file
 
-    # print(f"Main log type: {main_log_type}")
-    # print(f"Final key: {final_key}")
+    # Access continuationMarker from the nested combineMainLogsResult if it exists
+    combine_main_logs_result = data.get('combineMainLogsResult', {})
+    continuation_marker = combine_main_logs_result.get('continuationMarker', 0)
 
     if not log_files:
-        print("No files specified")
-        return
+        logger.info("No files specified")
+        return {'status': 'no-op'}
     
-    # print(f"Log files: {log_files}")
+    start_time = time.time()
+    remaining_time = context.get_remaining_time_in_millis() / 1000.0  # Convert to seconds
+
+    # if not main_log_type or continuation_marker != 0:
+    #     logger.info(f"Final key: {final_key}")
+    #     logger.info(f"Number of aggregate files: {len(log_files)}")
+    #     logger.info(f"Continuation marker: {continuation_marker}")
 
     # As multipart uploads require that all files but the last one be >= 5MB, we need to 
     # upload a file of this size to the scratchpad temp bucket as a starting point. The 
     # key used is the same as that of the final merged file.
-    # print("Uploading 5 MB accumulation file...")
-
-    with open(filler_file_path, 'rb') as f:
-        s3_client.upload_fileobj(f, TMP_LOGS_BUCKET_NAME, final_key)
+    if continuation_marker == 0:  # Only upload the filler file if starting from the beginning
+        with open(filler_file_path, 'rb') as f:
+            s3_client.upload_fileobj(f, TMP_LOGS_BUCKET_NAME, final_key)
 
     # We now perform a series of two-file multipart uploads, one for each log file we need to
     # aggregate.
-    for log_file in log_files:
+    # Resume or start the aggregation process
+    for index, log_file in enumerate(log_files[continuation_marker:], start=continuation_marker):
+        # if continuation_marker != 0:
+        #     logger.info(f"Index: {index}, Log file: {log_file}")
 
-        # print(f"Processing {log_file}...")
+        # Check if there is enough time left to process another file
+        elapsed_time = time.time() - start_time
+        if (remaining_time - elapsed_time) < 120:  # Less than 2 minutes left
+            logger.info(f"Lambda might time out, returning continuationMarker for next invocation: {index}")
+            return {'continuationMarker': index}  # Return the index of the next file to process
 
         if not aggregatable(log_file, main_log_type):
-            # print("  - not aggregatable")
             continue
 
+        # if not main_log_type or continuation_marker != 0:
+        #     logger.info(f"Aggregating index {index}: {log_file}...")
+
+        # Start timing the aggregation for this file
+        # file_start_time = time.time()
+
         # Initiate the multipart upload
-        # print("Initiating multipart upload...")
         mpu = s3_client.create_multipart_upload(Bucket=TMP_LOGS_BUCKET_NAME, Key=final_key)
             
         part_responses = []
@@ -82,7 +107,6 @@ def lambda_handler(data, _context):
             )
 
         # Finish the multipart upload for this log file to the temp bucket.
-        # print("Finishing multipart upload...")
         s3_client.complete_multipart_upload(
             Bucket=TMP_LOGS_BUCKET_NAME,
             Key=final_key,
@@ -90,21 +114,24 @@ def lambda_handler(data, _context):
             UploadId=mpu['UploadId']
         )
 
+        # Calculate and print the time taken to aggregate this file
+        # file_elapsed_time = time.time() - file_start_time
+        # if not main_log_type or continuation_marker != 0:
+        #     logger.info(f"Time elapsed: {file_elapsed_time} seconds")
+
+
     # All log files have now been added to the dummy file in the temp bucket.
     # Get the size of the result (which includes the +5MB dummy bytes)
     total_bytes = s3_resource.Object(TMP_LOGS_BUCKET_NAME, final_key).content_length
-    #print(f"Total bytes: {total_bytes}")
 
     # Initiate the final move of the result from the temp bucket to the chosen destination 
     # bucket, and also store the result using the Standard Infrequent Access storage class.
-    #print("Initiating final move multipart upload...")
     mpu = s3_client.create_multipart_upload(
         Bucket=dest_bucket_name, 
         Key=final_key,
         StorageClass='STANDARD_IA'
     )            
     # All we need here is a single part consisting of everything except the filler bytes.
-    #print("Uploading final move part copy...")
     response = s3_client.upload_part_copy(
         Bucket=dest_bucket_name,
         CopySource={'Bucket': TMP_LOGS_BUCKET_NAME, 'Key': final_key},
@@ -114,7 +141,6 @@ def lambda_handler(data, _context):
         CopySourceRange=f'bytes={FIVE_MB}-{total_bytes-1}'
     )
     # Do the upload
-    #print("Completing the final move multipart upload...")
     s3_client.complete_multipart_upload(
         Bucket=dest_bucket_name,
         Key=final_key,
@@ -129,16 +155,15 @@ def lambda_handler(data, _context):
         UploadId=mpu['UploadId']
     )
     # The final result is now in place in the destination bucket.
-    # final_total_bytes = s3_resource.Object(dest_bucket_name, final_key).content_length
-    # print(f"Final total bytes: {final_total_bytes}")
-    # if (final_total_bytes == 1):
-    #     print("ONE BYTE FILE RESULT")
 
     # Delete the versionless merged version from the temp bucket
     s3_client.delete_object(
         Bucket=TMP_LOGS_BUCKET_NAME,
         Key=final_key,
     )
+
+    # Return status
+    return {'status': 'done'}
 
 
 def get_files(thing):
